2fb83d8c637bd90b2672bea38d3c2c4fa1d18b13
rayonesh <rayanamacarona@gmail.com>
2026-06-26 16:31:52 +0400

    Creat Login pg NOT functional

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,5 +1,6 @@
+import.meta.env
 import { createClient } from '@supabase/supabase-js'
-
+7
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
